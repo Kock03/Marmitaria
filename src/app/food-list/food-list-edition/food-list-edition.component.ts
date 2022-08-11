3e0b5a878f981f6c5759cbd7e81178e4d7ad825c
VINICIUS KOCK <vinikk03@gmail.com>
2022-08-11 10:29:21 -0300

    edit food

--- src/app/food-list/food-list-edition/food-list-edition.component.ts
+++ src/app/food-list/food-list-edition/food-list-edition.component.ts
@@ -1,6 +1,8 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from "@angular/core";
+import { Component, ElementRef, EventEmitter, Inject, Input, Output, ViewChild } from "@angular/core";
 import { Food } from "src/app/shared/food.model";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { BehaviorSubject } from "rxjs";
 
 @Component({
     selector : 'app-food-list-edition',
@@ -11,6 +13,15 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 export class FoodListEditionComponent {
 
+    food!: any;
+    method: string = '';
+
+    // a ! significa que o elemento vai começar sem valor
+    @ViewChild("nameInput") nameInputRef!: ElementRef;
+    @ViewChild("amountInput") amountInputRef!: ElementRef;
+    @Output() foodAdded = new EventEmitter<Food>();
+    @Output() foodEdited = new EventEmitter<Food>();
+    @Input('edition')  foodEdit!: BehaviorSubject<any>; 
     
     meuFormGroup: FormGroup;
     constructor(private formBuilder: FormBuilder){
@@ -20,23 +31,40 @@ export class FoodListEditionComponent {
         });
     }
 
-    // a ! significa que o elemento vai começar sem valor
-    @ViewChild("nameInput") nameInputRef!: ElementRef;
-    @ViewChild("amountInput") amountInputRef!: ElementRef;
-    @Output() foodAdded = new EventEmitter<Food>();
-
+    ngOnInit(): void{
+        this.foodEdit.subscribe(res => {
+            if(res){
+                this.edition(res)
+            }
+        });
+    }
+    
+    
     addFood(){
-        if(!this.meuFormGroup.valid){ 
-            return;
-        } else{
+        this.method = sessionStorage.getItem('method')!;
+
+        if(this.method === 'edit'){
             const foodName = this.nameInputRef.nativeElement.value;
             const foodAmount = this.amountInputRef.nativeElement.value;
             const newFood = new Food(foodName, foodAmount);
-            this.foodAdded.emit(newFood);
+            this.foodEdited.emit(newFood);
             this.meuFormGroup.reset();
+        }else{
+
+            if(!this.meuFormGroup.valid){ 
+                return;
+            } else{
+                const foodName = this.nameInputRef.nativeElement.value;
+                const foodAmount = this.amountInputRef.nativeElement.value;
+                const newFood = new Food(foodName, foodAmount);
+                this.foodAdded.emit(newFood);
+                this.meuFormGroup.reset();
+            }
         }
     }
 
-   
-
+    edition(data: any){
+        this.meuFormGroup.controls['name'].setValue(data.name);
+        this.meuFormGroup.controls['amount'].setValue(data.amount);
+    }
 }
--- src/app/food-list/food-list.component.ts
+++ src/app/food-list/food-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BehaviorSubject } from 'rxjs';
 import { Lunch } from '../lunch/lunch.model';
 import { Food } from '../shared/food.model';
 
@@ -15,16 +16,31 @@ export class FoodListComponent implements OnInit {
   @Input() delete: EventEmitter<any> = new EventEmitter();
 
   foodList: Food[] = [];
+  foodEdit!: BehaviorSubject<any>; 
 
     @Inject(MAT_DIALOG_DATA) public data: any
     constructor() { }
 
   ngOnInit(): void {
+    this.foodEdit = new BehaviorSubject<any>('');
   }
 
   onFoodAdded(food: Food){
     this.foodList.push(food);
-    sessionStorage.setItem('index', this.index.toString());
+    // sessionStorage.setItem('index', this.index.toString());
+  }
+
+  onFoodEdited(food: Food){
+    this.index = this.foodList.findIndex((lunch) => food.name === lunch.name);
+    this.foodList.splice(this.index, 1);
+    this.foodList.push(food);
+    sessionStorage.setItem('method', '');
+    // sessionStorage.setItem('index', this.index.toString());
+  }
+
+  edit(data: any){
+    sessionStorage.setItem('method', 'edit');
+    this.foodEdit.next(data)
   }
 
   onDelete(food: any){
