a389cb4f02bbeaca3417c1f5f3021cdc8e39189c
VINICIUS KOCK <vinikk03@gmail.com>
2022-09-05 17:00:55 -0300

    bag almostmost complete

--- src/app/lunch/lunch-details/lunch-review-dialog/lunch-review-dialog.component.ts
+++ src/app/lunch/lunch-details/lunch-review-dialog/lunch-review-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { timeStamp } from 'console';
 import { Lunch } from '../../lunch.model';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { doc, getDoc } from "firebase/firestore";
 
 @Component({
   selector: 'app-lunch-review-dialog',
@@ -16,9 +18,10 @@ export class LunchReviewDialogComponent implements OnInit {
   bagValueBag!: any;
   bagLenght!: number;
   bagImage!: any;
+  addNumber !: any;
+  i!: any;
 
-
-  constructor() { }
+  constructor(private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
     this.bagValue = JSON.parse(sessionStorage.getItem('value')!);
@@ -26,13 +29,36 @@ export class LunchReviewDialogComponent implements OnInit {
     this.bagValueBag = JSON.parse(sessionStorage.getItem('valueBag')!);
     this.bagImage = JSON.parse(sessionStorage.getItem('imageLink')!);
     this.bagLenght = JSON.parse(sessionStorage.getItem('lenght')!);
-    
+    this.addNumber = 1;
   }
 
   removeFoodBag(i: any){
-    this.bagValue = this.bagValue - this.bagValueBag[i];
-    this.bagImage.splice(i, 1);
-    this.bagName.splice(i, 1);
-    this.bagValueBag.splice(i, 1);
+    let remove = document.getElementById('remove') as HTMLDivElement;
+    let bagRemove = document.getElementById('bagRemove') as HTMLDivElement;
+    if(this.addNumber == 1){
+      this.bagValue = this.bagValue - this.bagValueBag[i];
+      this.bagImage.splice(i, 1);
+      this.bagName.splice(i, 1);
+      this.bagValueBag.splice(i, 1);
+    }else if(this.addNumber == 2){
+      bagRemove.style.display = 'none';
+      remove.style.display = 'block';
+    }else{
+      bagRemove.style.display = 'block';
+      remove.style.display = 'none';
+    }
+    this.addNumber -= 1;
+    this.bagValueBag[i] = this.bagValue * this.addNumber;
+  }
+
+  addFoodBag(i: any){
+    let remove = document.getElementById('remove') as HTMLDivElement;
+    let bagRemove = document.getElementById('bagRemove') as HTMLDivElement;
+    if(this.addNumber == 1){
+      remove.style.display = 'none';
+      bagRemove.style.display = 'block';
+    }
+    this.addNumber += 1;
+    this.bagValueBag[i] = this.bagValue * this.addNumber;
   }
 }
