711c14379b13c0f2d1ddebe453264f7578773703
VINICIUS KOCK <vinikk03@gmail.com>
2022-09-29 08:52:17 -0300

    heroku-test

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/restaurante'));
+app.use(express.static('./src'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/restaurante/'}),
+    res.sendFile('index.html', {root: 'src'}),
 );
 
 // Start the app by listening on the default Heroku port
